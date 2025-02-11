722070498b047ab3cc7f68d6761af354cbf717d5
altafxx <altafhasnan@gmail.com>
2025-02-11 23:14:15 +0800

    fix: remove hardcode video path

--- src/app/(web)/[id]/page.tsx
+++ src/app/(web)/[id]/page.tsx
@@ -3,12 +3,11 @@ import { fetchVideoByID } from "../action";
 
 export default async function VideoByID({ id }: { id: number }) {
     const video = await fetchVideoByID(id);
-
     return (
         <main className="flex flex-col min-h-screen place-items-center py-24">
             <div className="font-bold text-2xl">{video?.title}</div>
             <div className="max-w-5xl rounded-md overflow-clip">
-                <VideoPlayer src={"http://video.danielaltaf.dev/video/Config/1720964050725.json/master.m3u8"} />
+                <VideoPlayer src={video.path} />
             </div>
             <div>{video?.description}</div>
 
