ecb9ae741f1a0a7c96fb40f81477362d68f7cec0
altafxx <altafhasnan@gmail.com>
2025-02-17 02:07:46 +0800

    feat: api route

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -1,14 +1,29 @@
+import { fetchVideoByID, fetchVideos } from '@/app/action/video'
+import { fetchFolders } from '@/app/action/folder'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-    return c.json({
-        message: 'Hello from Hono!'
-    })
+app.get('/videos', async (c) => {
+    const videos = await fetchVideos()
+
+    return c.json(videos)
+})
+
+app.get('/videos/:id', async (c) => {
+    const video = await fetchVideoByID(+c.req.param('id'))
+
+    return c.json(video)
+})
+
+app.get('folder', async (c) => {
+    const folder = await fetchFolders()
+
+    return c.json(folder)
 })
 
 export const GET = handle(app)
+// export const POST = handle(app)
