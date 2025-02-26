533c60c58b336dc91ce7297c5ba99065e74559ef
altafxx <altafhasnan@gmail.com>
2025-02-26 21:56:56 +0800

    fix: remote pattern, args on video dockerfile

--- src/lib/next-config/allowed-remote-pattern.ts
+++ src/lib/next-config/allowed-remote-pattern.ts
@@ -17,20 +17,31 @@ const defaultRemoteImages = [
 
 export default function allowedRemotePattern(): RemotePattern[] {
     const allowRemoteImagesEnv = process.env.ALLOWED_REMOTE_IMAGES || "";
-    const allowRemoteImagesArray = allowRemoteImagesEnv.split(",") || [];
-    const allowRemoteImagesURL = allowRemoteImagesArray.map((remoteImage: string) => {
-        const url = new URL(remoteImage)
-        return {
-            protocol: url.protocol?.replace(":", "") === "https" ? "https" : "http",
-            hostname: url.hostname,
-            port: url.port,
-            pathname: url.pathname.charAt(-1) === "/" || url.pathname === "/" ? url.pathname + "**" : url.pathname + "/**"
-        } satisfies RemotePattern;
-    })
+    const allowRemoteImagesArray = allowRemoteImagesEnv.split(",").filter(url => url.trim() !== "");
+
+    const allowRemoteImagesURL = () => {
+        if (allowRemoteImagesArray.length === 0) return null;
+
+        return allowRemoteImagesArray.map((remoteImage: string) => {
+            try {
+                const url = new URL(remoteImage.trim());
+                const pattern: RemotePattern = {
+                    protocol: url.protocol.replace(":", "") === "https" ? "https" : "http",
+                    hostname: url.hostname,
+                    port: url.port,
+                    pathname: url.pathname.charAt(-1) === "/" || url.pathname === "/" ? url.pathname + "**" : url.pathname + "/**"
+                };
+                return pattern;
+                /* eslint-disable @typescript-eslint/no-unused-vars */
+            } catch (_) {
+                console.warn(`Invalid URL: ${remoteImage}`);
+                return null;
+            }
+        }).filter((pattern): pattern is RemotePattern => pattern !== null);
+    };
 
     return [
         ...defaultRemoteImages,
-        ...allowRemoteImagesURL
+        ...(allowRemoteImagesURL() || [])
     ];
-
 }
