8a9ccca5cec2532fa94c16ee48795d5c5d332591
altafxx <altafhasnan@gmail.com>
2025-03-29 07:04:03 +0800

    feat: default value for localhost

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,16 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: allowedRemotePattern() as RemotePattern[],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  env: {
+    LOCALHOST: process.env.LOCALHOST || 'false',
+  },
 };
 
 export default nextConfig;
